96369ba43ca1e4452dc72a522c7b62d7b89f764c
Angga Afrianto <anggaaurelius@gmail.com>
2021-02-07 22:58:54 +0700

    auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { authenticated } = require("../middlewares/auth");
 
 const { 
     register, login
@@ -21,9 +22,9 @@ router.delete("/user/:id", deleteUser);
 
 router.get("/books", getBooks);
 router.get("/book/:id", getBooksById);
-router.post("/book", addBook);
-router.patch("/book/:id", editBook);
-router.delete("/book/:id", deletebook);
+router.post("/book", authenticated, addBook);
+router.patch("/book/:id", authenticated, editBook);
+router.delete("/book/:id", authenticated, deletebook);
 
 // auth
 router.post("/register", register);
