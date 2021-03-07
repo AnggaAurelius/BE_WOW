b71a151093ada2601a323878770e60ea00c2956c
Angga Afrianto <anggaaurelius@gmail.com>
2021-03-07 16:00:22 +0700

    Update uploadFile.js

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -21,9 +21,9 @@ exports.uploadFiles = (imageFile, bookFile) => {
         // dan hanya mengizikan jenis file type epub dan saat diupload beri ext .epub
         return {
           folder: "epub",
-          // resource_type: "raw",
+          resource_type: "raw",
           allowedFormats: ["epub"],
-          // format: "epub",
+          format: "epub",
         };
       } else if (
         file.fieldname === "thumbnail" ||
