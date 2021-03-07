92382ea3c9581afa4125de9ecdac225a0c3493f6
Angga Afrianto <anggaaurelius@gmail.com>
2021-03-07 16:33:16 +0700

    Update addTransaction.js

--- src/controllers/addTransaction.js
+++ src/controllers/addTransaction.js
@@ -10,37 +10,38 @@ exports.addTransaction = async (req, res) => {
       paymentStatus: "Pending",
     });
 
-    const transaction = await Transaction.findOne({
-      where: {
-        transferProof: newTransaction.transferProof,
-      },
-      include: {
-        as: "users",
-        model: User,
-        attributes: {
-          exclude: ["email", "password", "createdAt", "updatedAt"],
-        },
-      },
-      attributes: {
-        exclude: ["userId", "createdAt", "updatedAt"],
-      },
-    });
+    // const transaction = await Transaction.findOne({
+    //   where: {
+    //     transferProof: newTransaction.transferProof,
+    //   },
+    //   include: {
+    //     as: "users",
+    //     model: User,
+    //     attributes: {
+    //       exclude: ["email", "password", "createdAt", "updatedAt"],
+    //     },
+    //   },
+    //   attributes: {
+    //     exclude: ["userId", "createdAt", "updatedAt"],
+    //   },
+    // });
     res.send({
       data: {
-        transaction: {
-          id: transaction.id,
-          user: transaction.users,
-          transferProof: transaction.transferProof,
-          remainingActive: transaction.remainingActive,
-          userStatus: transaction.userStatus,
-          paymentStatus: transaction.paymentStatus,
-        },
+        newTransaction,
+        // : {
+        //   id: transaction.id,
+        //   user: transaction.users,
+        //   transferProof: transaction.transferProof,
+        //   remainingActive: transaction.remainingActive,
+        //   userStatus: transaction.userStatus,
+        //   paymentStatus: transaction.paymentStatus,
+        // },
       },
     });
   } catch (err) {
     console.log(err);
     res.status(500).send({
-      message: "Server Error",
+      message: "Server Error..",
     });
   }
 };
